0a3f1a4b18aa54d8a5600ae19ff9e0b731274286
karen-edith <karen.edith.garcia1@gmail.com>
2018-08-07 19:40:30 -0700

    Navigation Bar

---
+++ website/src/components/HomePage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './HomePage.css';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <div className='navbarContainer'>
+          <Navbar fluid className='navigationbar'>
+            <Navbar.Header>
+              <Navbar.Brand className='logo'> Hardwood Floors </Navbar.Brand>
+            </Navbar.Header>
+            <Nav pullRight>
+              <NavItem className='nbitem'> HOME </NavItem>
+              <NavItem className='nbitem'> ABOUT US </NavItem>
+              <NavItem className='nbitem'> SERVICES </NavItem>
+              <NavItem className='nbitem'> PORTFOLIO </NavItem>
+              <NavItem className='nbitem'> CONTACT US </NavItem>
+              <NavItem className='nbitem'> <div className='licnbitem'> <div className='lcinner'> LIC # 345678 </div> </div> </NavItem>
+            </Nav>
+          </Navbar>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default HomePage;
