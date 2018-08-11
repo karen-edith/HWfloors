8f2e5ea16bf1a5d687551d6607920c82acfc9921
karen-edith <karen.edith.garcia1@gmail.com>
2018-08-11 11:39:06 -0700

    styling for break points

--- website/src/components/NavBar.jsx
+++ website/src/components/NavBar.jsx
@@ -8,11 +8,11 @@ class NavBar extends Component{
       <div>
         <div className='navbarContainer'>
           <Navbar fluid className='navigationbar'>
-            <Navbar.Header>
+            <Navbar.Header className='header-brand'>
               <Navbar.Brand className='logo'> Hardwood Floors </Navbar.Brand>
             </Navbar.Header>
             <Nav pullRight>
-              <NavItem className='nbitem'> HOME </NavItem>
+              <NavItem className='nbitem nbitemtop'> HOME </NavItem>
               <NavItem className='nbitem'> ABOUT US </NavItem>
               <NavItem className='nbitem'> SERVICES </NavItem>
               <NavItem className='nbitem'> PORTFOLIO </NavItem>
