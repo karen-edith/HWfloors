3c629ab181aaaf458e9d905738b4669155927962
karen-edith <karen.edith.garcia1@gmail.com>
2018-08-11 10:41:55 -0700

    added router

--- website/src/index.js
+++ website/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import HomePage from './pages/HomePage';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-ReactDOM.render(<HomePage />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path='/' component={HomePage}></Route>
+    </div>
+  </Router> , document.getElementById('root'));
 registerServiceWorker();
