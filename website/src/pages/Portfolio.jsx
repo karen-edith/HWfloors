15ce6e6baa30281bff5e0faa519588e242e674dd
karen-edith <karen.edith.garcia1@gmail.com>
2018-08-21 11:09:00 -0700

    added gallery

--- website/src/pages/Portfolio.jsx
+++ website/src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Grid, Row, Col, Image} from 'react-bootstrap';
 import './Portfolio.css';
 
 class Portfolio extends Component {
@@ -9,6 +9,43 @@ class Portfolio extends Component {
     return(
       <div>
         <NavBar/>
+        <div className='galleryContainer'>
+          <Grid className='singlePictureGrid' fluid>
+            <Row className='singlePictureRow'>
+              <Col className='singlePictureCol'>
+                <Image className='singlePhoto' src={require('../images/galleryPhoto1.jpg')} responsive />
+              </Col>
+            </Row>
+          </Grid>
+          <Grid className='galleryPictureGrid' fluid>
+            <Row className='pictureRow'>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto1.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto2.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto3.jpg')} responsive/></Col>
+            </Row>
+            <Row className='pictureRow'>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto4.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto5.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto6.jpg')} responsive/></Col>
+            </Row>
+            <Row className='pictureRow'>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto5.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto1.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto4.jpg')} responsive/></Col>
+            </Row>
+            <Row className='pictureRow'>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto2.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto4.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto3.jpg')} responsive/></Col>
+            </Row>
+            <Row className='pictureRow'>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto4.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto1.jpg')} responsive/></Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} className='photoCol'><Image className='photo' src={require('../images/galleryPhoto6.jpg')} responsive/></Col>
+            </Row>
+          </Grid>
+        </div>
+
         <Footer/>
       </div>
     )
