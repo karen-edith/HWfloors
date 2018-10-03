c920c1f078321a887f66664d5d224c2e8f12231a
karen-edith <karen.edith.garcia1@gmail.com>
2018-10-02 23:29:00 -0700

    changed carousel code

--- website/src/pages/Services.jsx
+++ website/src/pages/Services.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import {Grid, Col, Row Image, Glyphicon} from 'react-bootstrap';
+import {Grid, Col, Row, Image, Glyphicon} from 'react-bootstrap';
 import './Services.css';
 import installing from '../images/Installation.png';
 import repair from '../images/repair.jpg';
@@ -12,7 +12,8 @@ class Services extends Component {
   constructor(props){
     super(props)
     this.state = {
-      photoArray: [installing, finishing, repair]
+      photoArray:[installing, finishing, repair],
+      currentPhoto: installing
     }
   }
 
@@ -47,9 +48,33 @@ class Services extends Component {
         </Grid>
         <Grid className='pictureGrid' fluid>
           <Row>
-            <Image src={this.state.photoArray[0]} responsive/>
-            <div className='leftArrow'> <Glyphicon glyph='chevron-left'/></div>
-            <div className='rightArrow'> <Glyphicon glyph='chevron-right'/></div>
+            <Image src={this.state.currentPhoto} responsive/>
+            <div className='leftArrow' onClick={()=>{
+              this.state.photoArray.map((item, index)=>{
+                if(item === this.state.currentPhoto) {
+                  if(index !== 0) {
+                    let test = this.state.photoArray.slice()
+                    this.setState({currentPhoto: test[index-1]})
+                  } else if (index === 0) {
+                    let test = this.state.photoArray.slice()
+                    this.setState({currentPhoto: test[2]})
+                  }
+                }
+              })
+            }}> <Glyphicon glyph='chevron-left'/></div>
+            <div className='rightArrow' onClick={()=>{
+              this.state.photoArray.map((item, index)=>{
+                if(item === this.state.currentPhoto) {
+                  if(index !== 2) {
+                    let test = this.state.photoArray.slice()
+                    this.setState({currentPhoto: test[index+1]})
+                  } else if (index === 2) {
+                    let test = this.state.photoArray.slice()
+                    this.setState({currentPhoto: test[0]})
+                  }
+                }
+              })
+            }}> <Glyphicon glyph='chevron-right'/></div>
           </Row>
         </Grid>
         <Footer/>
